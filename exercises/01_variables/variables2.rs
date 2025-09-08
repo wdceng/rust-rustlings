d40164e147e8870a84632895e888680b8c7582f0
Davor Šercer <davor.sercer@gmail.com>
2025-09-08 21:34:46 +0200

    Initial commit

---
+++ exercises/01_variables/variables2.rs
@@ -0,0 +1,12 @@
+use std::arch::aarch64::int16x4x3_t;
+
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = 10;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
